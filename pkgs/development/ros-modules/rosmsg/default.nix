d8d721ecc69e30714f2572456112442c1ea43085
tubleronchik <m.bystramovich@gmail.com>
2021-07-11 23:23:47 +0300

    upd rosmsg hash

--- pkgs/development/ros-modules/rosmsg/default.nix
+++ pkgs/development/ros-modules/rosmsg/default.nix
@@ -19,7 +19,7 @@ in mkRosPackage {
     owner = "ros-gbp";
     repo = "ros_comm-release";
     rev = "release/${rosdistro}/${pname}/${version}-0";
-    sha256 = "02rwqa874v034r42b5kia40z3rvfrly801yablqygl0v0ffzia1g";
+    sha256 = "1wsn3crjiz7rnjfb4mdj5k2qnjflqi1gajcq870s0yfdrx1mvmf7";
   };
 
   propagatedBuildInputs = [ catkin genmsg rosbag roslib ];
