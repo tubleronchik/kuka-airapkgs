3bb7d5c20b134e14993c003751f66ee1a8d282a0
tubleronchik <m.bystramovich@gmail.com>
2021-07-12 00:09:35 +0300

    upd roscpp hash

--- pkgs/development/ros-modules/roscpp/default.nix
+++ pkgs/development/ros-modules/roscpp/default.nix
@@ -27,7 +27,7 @@ in mkRosPackage {
     owner = "ros-gbp";
     repo = "ros_comm-release";
     rev = "release/${rosdistro}/${pname}/${version}-0";
-    sha256 = "0adhvmxdz0fpc0svvd8agr9yppq6w8v2ylm7sak8xwgbr2zh227g";
+    sha256 = "0xapxgah3r3sbhljdnh9zf9njjiq06cgqg9wypmhrs65lmvnakkm";
   };
 
   propagatedBuildInputs =
