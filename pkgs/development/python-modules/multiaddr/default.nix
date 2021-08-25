2f6352e9f7db788101e889b52935e371becc4d50
tubleronchik <m.bystramovich@gmail.com>
2021-08-25 16:58:20 +0300

    upd multiadr

--- pkgs/development/python-modules/multiaddr/default.nix
+++ pkgs/development/python-modules/multiaddr/default.nix
@@ -1,26 +1,36 @@
-{ stdenv
+{ lib
 , buildPythonPackage
 , fetchPypi
-, lib
 , netaddr
 , six
 , varint
 , base58
 , idna
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "multiaddr";
-  version = "0.0.8";
+  version = "0.0.9";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0f0jvdsnqvc0mfhy6dl82jm3kyfcpkwc2bfx91mzwicr8y5wdbig";
+    sha256 = "1kqfmcbv8plpicbygwpdljin7n82iyxklc0w1ywxbhzdi58nkcih";
   };
+
+  postPatch = ''
+    substituteInPlace setup.py --replace "'pytest-runner'," ""
+  '';
+
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "multiaddr" ];
 
   propagatedBuildInputs = [ netaddr six varint base58 idna ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "multiaddr implementation in Python";
     homepage = https://github.com/multiformats/py-multiaddr;
     license = [licenses.mit licenses.asl20];
